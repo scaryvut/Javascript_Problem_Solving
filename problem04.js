05cc488dfc3f05eca8a26b942fe9e9980de09849
Arafatullah Arshad <arafatullaharshad@gmail.com>
2026-09-06 10:53:49 +0600

    Complete Problem04 which name Dhaka CNG Fare Meter

---
+++ problem04.js
@@ -0,0 +1,14 @@
+function getCngFare(distance, isNight=false, waitingMinutes=0){
+    let fare=50
+
+    if(distance>2){
+        fare +=(distance-2)*15
+    }
+    fare +=waitingMinutes*2
+
+    if(isNight){
+        fare=fare*1.20
+    }
+    return fare
+}
+console.log(getCngFare(5))
